529faa57808caf984d2cc341d3bbbc6759b6ae7e
millerribeiroo <millerrib@gmail.com>
2025-03-24 19:22:07 +0000

    Operadores lógicos e condicionais fixed.

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -100,7 +100,7 @@ int main() {
     scanf(" %d", &atributo);
 
     // Comparando População:
-    if (atributo = 1) {
+    if (atributo == 1) {
         if (pop1 > pop2) {
             printf("São Paulo (SP): %lu habitantes\n", pop1);
             printf("Rio de Janeiro (RJ): %lu habitantes\n", pop2);
@@ -113,7 +113,7 @@ int main() {
     }
 
     // Comparando Área:
-    if (atributo = 2) {
+    if (atributo == 2) {
         if (area1 > area2) {
             printf("São Paulo (SP): %.2f km² de área. \n", area1);
             printf("Rio de Janeiro (RJ): %.2f km² de área. \n", area2);
@@ -126,7 +126,7 @@ int main() {
     }
 
     // Comparando PIB:
-    if (atributo = 3) {
+    if (atributo == 3) {
         if (pib1 > pib2) {
             printf("São Paulo (SP): PIB de R$%ld\n", pib1);
             printf("Rio de Janeiro (RJ): PIB de R$%ld\n", pib2);
@@ -139,7 +139,7 @@ int main() {
     }
 
     // Comparando Pontos Turísticos:
-    if (atributo = 4) {
+    if (atributo == 4) {
         if (pontos1 > pontos2) {
             printf("São Paulo (SP): %d pontos turísticos\n", pontos1);
             printf("Rio de Janeiro (RJ): %d pontos turísticos\n", pontos2);
@@ -152,7 +152,7 @@ int main() {
     }
 
     // Comparando Densidade Populacional:
-    if (atributo = 5) {
+    if (atributo == 5) {
         if (densi1 > densi2) {
             printf("São Paulo (SP): %.2f hab/km²\n", densi1);
             printf("Rio de Janeiro (RJ): %.2f hab/km²\n", densi2);
@@ -165,7 +165,7 @@ int main() {
     }
 
     // Comparando PIB per capita:
-    if (atributo = 6) {
+    if (atributo == 6) {
         if (pibpercap1 > pibpercap2) {
             printf("São Paulo (SP): PIB per capta de R$%.2f\n", pibpercap1);
             printf("Rio de Janeiro (RJ): PIB per capta de R$%.2f\n", pibpercap2);
@@ -178,7 +178,7 @@ int main() {
     }
 
     // Comparando Super Poder:
-    if (atributo = 7) {
+    if (atributo == 7) {
         if (superpoder1 > superpoder2) {
             printf("São Paulo (SP): %.2f pontos de superpoder. \n", superpoder1);
             printf("Rio de Janeiro (RJ): %.2f pontos de superpoder. \n", superpoder2);
